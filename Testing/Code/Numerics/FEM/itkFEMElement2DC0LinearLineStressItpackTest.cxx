890b2b625ac0e399e836d58fad968d90e6da2f96
Vincent Magnotta <vincent-magnotta@uiowa.edu>
2011-02-02 17:29:27 -0600

    Updated the Copyright notice and removed MSC_VER pragmas for tests

--- Testing/Code/Numerics/FEM/itkFEMElement2DC0LinearLineStressItpackTest.cxx
+++ Testing/Code/Numerics/FEM/itkFEMElement2DC0LinearLineStressItpackTest.cxx
@@ -1,34 +1,27 @@
 /*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    itkFEMElement2DStrainTest.cxx
-  Language:  C++
-  Date: $Date$
-  Version:   $Revision$
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
-
-// disable debug warnings in MS compiler
-#ifdef _MSC_VER
-#pragma warning(disable: 4786)
-#endif
-
-#include "itkFEM.h"
 #include "itkFEM.h"
 #include "itkFEMLinearSystemWrapperItpack.h"
 #include "itkFEMLinearSystemWrapperVNL.h"
 #include "itkFEMSolver.h"
 
 #include <iostream>
-using std::ofstream;
-using std::ifstream;
 
 //  Example taken from 'Fundamentals of the Finite ELement Method' - Grandin
 int itkFEMElement2DC0LinearQuadrilateralStrainItpackTest(int argc, char *argv[])
@@ -36,7 +29,7 @@ int itkFEMElement2DC0LinearQuadrilateralStrainItpackTest(int argc, char *argv[])
   typedef itk::fem::Solver SolverType;
   typedef SolverType *     SolverPointerType;
   SolverPointerType m_Solver = new SolverType;
-  ifstream          fileInput;
+  std::ifstream          fileInput;
 
   itk::fem::LinearSystemWrapperItpack WrapperItpack;
   WrapperItpack.SetMaximumNonZeroValuesInMatrix(100);
